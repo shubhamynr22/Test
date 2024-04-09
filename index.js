b98be9aa8e411fc13aa7cd8ad60ff584a22bed65
credainshubham <shubham.gupta@credain.com>
2024-04-09 17:53:22 +0530

    feat : added simple authentication using jwt

--- index.js
+++ index.js
@@ -1,34 +1,82 @@
 const express = require('express');
-const fs = require('fs');
-const app = express();
-const path = require('path');
+const jwt = require('jsonwebtoken');
 
+const app = express();
+
+app.use(express.json());
+
+const jwtPassword = '123456';
 const PORT = 3000;
 
-app.get('/files', function (req, res) {
-  const filesPath = path.join(__dirname, 'files');
-  fs.readdir(filesPath, function (err, data) {
-    if (err) {
-      res.status(400).send('Error reading files');
-    } else {
-      res.status(200).send(data);
+const ALL_USERS = [
+  {
+    username: 'harkirat@gmail.com',
+    password: '123',
+    name: 'harkirat singh'
+  },
+  {
+    username: 'raman@gmail.com',
+    password: '123321',
+    name: 'Raman singh'
+  },
+  {
+    username: 'priya@gmail.com',
+    password: '123321',
+    name: 'Priya kumari'
+  }
+];
+
+function userExists(username, password) {
+  for (let i = 0; i < ALL_USERS.length; i++) {
+    if (ALL_USERS[i].username == username && ALL_USERS[i].password == password) {
+      return true;
     }
+  }
+  return false;
+}
+
+app.post('/signin', function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!userExists(username, password)) {
+    return res.status(403).json({
+      msg: 'User doesnt exist in our in memory db'
+    });
+  }
+
+  var token = jwt.sign({ username: username }, '123456');
+  return res.json({
+    token
   });
 });
 
-app.get('/files/:fileName', function (req, res) {
-  const name = req.params.fileName;
-  const filePath = path.join(__dirname, 'files', name);
-  console.log(filePath);
-
-  fs.readFile(filePath, 'utf8', function (err, data) {
-    if (err) {
-      console.log(err);
-      res.status(500).send('File not found');
-    } else {
-      console.log(data);
-      res.status(200).send(data);
+app.get('/users', function (req, res) {
+  const token = req.headers.authorization;
+  try {
+    const decoded = jwt.verify(token, jwtPassword);
+    const username = decoded.username;
+    const response = [];
+    for (let i = 0; i < ALL_USERS.length; i++) {
+      if (!(ALL_USERS[i].username == username)) {
+        response.push(ALL_USERS[i]);
+      }
     }
+    return res.status(200).json({
+      msg: 'Successfully verified',
+      data: response
+    });
+  } catch (err) {
+    return res.status(403).json({
+      msg: 'Invalid token'
+    });
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.log(`Error : ${err}`);
+  res.json({
+    msg: 'Something went wrong'
   });
 });
 
